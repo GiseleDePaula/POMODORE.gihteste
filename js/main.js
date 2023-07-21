ec60e14efba067edac2c15aec16f256fa2712d92
GiseleDePaula <depaula.gisele2023@gmail.com>
2023-07-21 19:40:58 -0300

    Disparar um aviso ao fim do cronometro

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-const segundosPomodoro = 4000 //25*60*1000
+const segundosPomodoro = 4000 // Estamos usando 4 segundos para testes. 0 tempo oficial do pomodoro 25*60*1000
 const disparador = document.querySelector('#disparador')
 
 disparador.addEventListener('click', () => {
     console.log("Disparador ativado.")
+
+    // Criando o cronometro
     setTimeout(() => {
         console.log("Finalizou o Pomodoro!");
+        alert("O seu tempo de produção do pomodoro acabou. Vá descansar!")
     }, segundosPomodoro)
 })
 
